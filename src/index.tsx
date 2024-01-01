e5fb08aaa2f283fbaf8e3403b1f11bd2a826e87d
José Carlos de Camargo <jccamargo15@gmail.com>
2024-01-01 14:00:02 -0300

    feat: Design System em React - Configuração do Stitches

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,11 @@
-import { colors } from '@ignite-ui/tokens'
+import { styled } from './styles'
 
-console.log(colors)
+const Button = styled('button', {
+  fontFamily: '$default',
+  backgroundColor: '$ignite500',
+  borderRadius: '$md',
+})
 
 export function App() {
-  return <h1 style={{ color: colors.ignite300 }}>Helo DS</h1>
+  return <Button>Helo DS</Button>
 }
