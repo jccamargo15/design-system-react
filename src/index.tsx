b5442b4faa1dcde3d18151ccbba614b4ebc03c28
José Carlos de Camargo <jccamargo15@gmail.com>
2024-01-02 22:42:36 -0300

    feat: Design System em React - Story: Button

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,31 @@
+import { ComponentProps } from 'react'
+
 import { styled } from './styles'
 
-const Button = styled('button', {
+export const Button = styled('button', {
   fontFamily: '$default',
-  backgroundColor: '$ignite500',
-  borderRadius: '$md',
+  backgroundColor: '$ignite300',
+  borderRadius: '$sm',
+  border: 0,
+  fontWeight: 'bold',
+  color: 'White',
+
+  variants: {
+    size: {
+      small: {
+        fontSize: 14,
+        padding: '$2 $4',
+      },
+      big: {
+        fontSize: 16,
+        padding: '$3 $6',
+      },
+    },
+  },
+
+  defaultVariants: {
+    size: 'small',
+  },
 })
 
-export function App() {
-  return <Button>Helo DS</Button>
-}
+export type ButtonProps = ComponentProps<typeof Button>
